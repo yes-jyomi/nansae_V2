eb3924e088d6a03cfce31fe9daf1d2e65781219f
Sagittta <js1verj@gmail.com>
2020-07-07 13:13:30 +0900

    front: 외부활동 리스트, 디테일, 추가 화면 완성

--- nansae_node/routes/outdoor.js
+++ nansae_node/routes/outdoor.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
     res.render('outdoor', { title: '외부활동' });
 });
 
+router.get('/detail', function(req, res, next) {
+    res.render('outdoor_detail', { title: '외부활동' });
+});
+
+router.get('/add', function(req, res, next) {
+    res.render('outdoor_add', { title: '외부활동' });
+});
+
 module.exports = router;
